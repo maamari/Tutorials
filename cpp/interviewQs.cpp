5af235ed36f4d3e3aa4a9df17a5105e62909e94f
maamari <maamari@usc.edu>
2020-12-11 21:47:20 -0600

    add twoNumSum

--- cpp/interviewQs.cpp
+++ cpp/interviewQs.cpp
@@ -13,6 +13,7 @@ vector<int> twoSum(vector<int>& nums, int target){
             for(int j=0; j<result.size(); j++){
                 cout << result[j] << ' ';
             }
+            cout << endl;
             return result;
         }
         num2s[nums[i]]=i; 
@@ -20,10 +21,46 @@ vector<int> twoSum(vector<int>& nums, int target){
     return {-1,-1};
 }
 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+ListNode* addTwoNums(ListNode* l1, ListNode* l2){
+    ListNode *head = new ListNode(0);
+    ListNode *result = head;
+    int carry = 0;
+    while(l1 || l2 || carry){
+        int v1 = 0, v2 = 0, v3 = 0;
+        if(l1){
+            v1 = l1->val;
+            l1 = l1->next;
+        }
+        if(l2){
+            v2 = l2->val;
+            l2 = l2->next;
+        }
+        v3 = v1+v2+carry;
+        carry = v3/10;
+        result->next = new ListNode(v3%10);
+        result = result->next;
+    }
+    cout << head->next->val << endl;
+    delete head;
+    delete result;
+    //return head->next;
+}
+
 int main(){
+
+    // Two sum   
     vector<int> nums{1,2,3,4,5};
     int target = 6;
     twoSum(nums, target);
+
+    // Add two nums
+    addTwoNums(new ListNode(1), new ListNode(2));
 
     return 0;
 }
--- python/interviewQs.py
+++ python/interviewQs.py
@@ -20,4 +20,37 @@ def twoSum(nums, target):
             num2s[num]=i
     return 0
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def addTwoNums(l1, l2):
+    '''
+    Problem: Given two non-empty linked lists
+    with digits stored in reverse order, add
+    the numbers represented by the lls and 
+    return the sum
+
+    Solution: For each ll node, add values
+    and carry, mod result by 10 and store
+    to next node, divide by 10 and store 
+    to carry over, return first node
+    '''
+    topNode = result = ListNode(0)
+    carry = 0
+    while l1 or l2 or carry:
+        if l1:
+            v1 = l1.val
+            l1 = l1.next
+        if l2:
+            v2 = l2.val
+            l2 = l2.next
+        v3 = v1+v2+carry
+        carry = v3//10
+        result.next = ListNode(v3)
+    return topNode.next
+
+####################################################################
+
 print(twoSum([1,2,3,4,5],6))
+print(addTwoNums(ListNode(val=2), ListNode(val=2)).val)
